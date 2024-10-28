0aca8f5750fff8806851d6de4df9e854cf4a995d
AbdulkarimBa <abdulkarim.balta@icarasia.com>
2024-10-28 17:16:36 +0800

    log

--- functions/setcookies.js
+++ functions/setcookies.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
     const url = new URL(context.request.url);
 
     // log the whole request
-    console.table('Request to set cookies on foreign app:', context.request);
+    console.log('Request to set cookies on foreign app:', context.request);
     //const token = url.searchParams.get("token");
 
     // get request headers
